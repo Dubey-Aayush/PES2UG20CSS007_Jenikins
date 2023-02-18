328109a1ee27f2d24172cf265a5ef0cd900a4cb4
Aayush Dubey <91554151+Dubey-Aayush@users.noreply.github.com>
2023-02-18 16:03:05 -0500

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I PES2UG20CS007 have successfully built and run\n";
-  couut<<"This line has been added for the task"<<endl;
+  couut<<"This line has been added"<<endl;
   return 0;
 }
